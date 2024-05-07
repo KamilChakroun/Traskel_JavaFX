6e202e961bab396c63711cb5aad49c1b677757f5
KamilChak <odragon707@gmail.com>
2024-05-07 01:39:01 +0100

    .

--- src/main/java/Controllers/DashboardController.java
+++ src/main/java/Controllers/DashboardController.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 public class DashboardController {
+
+    @FXML
+    private void livraisonCtrl(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Livraisons.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- src/main/java/com/pi/traskel/Main.java
+++ src/main/java/com/pi/traskel/Main.java
@@ -9,9 +9,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("/Fxml/Livraisons.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("/Fxml/Dashboard.fxml"));
         Scene scene = new Scene(parent);
-        stage.setTitle("LIVRAISONS");
+        stage.setTitle("Admin Dashboard");
         stage.setScene(scene);
         stage.show();
 
